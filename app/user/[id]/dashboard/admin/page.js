2c98142c445be3f06a1320f6111f3fd2e87165e9
Giovanni Salcuni <giovannisalcuni@MacBook-Pro-de-Giovanni.local>
2024-05-06 18:04:30 +0200

    Recuperation des donnes dans admin

--- app/user/[id]/dashboard/admin/page.js
+++ app/user/[id]/dashboard/admin/page.js
@@ -1,17 +1,26 @@
 'use client'
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuthContext } from '@/app/providers/AuthProvider';
 import { redirect } from 'next/navigation';
+import getMultipleData from '@/app/db/request/getMultipleData';
 
 export default function Page() {
     const { user, isAdmin } = useAuthContext();
+    const [articles, setArticles] = useState([]);
 
     useEffect(() => {
         if(!isAdmin){
-            redirect('/')
+            return redirect('/')
+        } else {
+            getMultipleData("article")
+            .then(result => {
+                const { resultGetMultipleData } = result;
+                setArticles(resultGetMultipleData)
+            })
         }
-    })
+    }, [])
 
+    console.log(articles)
     return <h1> Bienvenue {user.email}</h1>
 }
