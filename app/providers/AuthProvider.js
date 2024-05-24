0b9ac45d8a666a68a7326718092484ebaa789651
Salcuni <giovanni.salcuni@palexpo.ch>
2024-05-24 11:24:47 +0200

    Amelioration du provider user

--- app/providers/AuthProvider.js
+++ app/providers/AuthProvider.js
@@ -1,39 +1,38 @@
-'use client'
 
+"use client"
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import { getCookie, setCookie } from 'cookies-next';
 import Spinner from '../components/Spinner';
-
 
 export const AuthContext = createContext({});
 
 export const useAuthContext = () => useContext(AuthContext);
 
 export const AuthContextProvider = ({ children }) => {
-    const [user, setUser] = useState(false);
+    const [user, setUser] = useState(null);
     const [isAdmin, setIsAdmin] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
 
     const userLogged = (data) => {
-        setUser(data)
-        setCookie("user", data)
-    }
-
+        setUser(data);
+        setCookie("user", data);
+    };
 
     useEffect(() => {
-        setIsLoading(true);
         const userStored = getCookie('user');
         if (userStored) {
-            userLogged(JSON.parse(userStored))
-            setIsLoading(false);
-        };
-        setIsLoading(false);
+            userLogged(JSON.parse(userStored));
+        }
+        setIsLoading(false); // Met fin au chargement une fois que la vérification est terminée, qu'il y ait un utilisateur ou non
     }, []);
-
 
     return (
         <AuthContext.Provider value={{ user, isAdmin, userLogged }}>
-            {isLoading ? <Spinner message="Récuperation des données ..."/> : children}
+            {isLoading && !user ? (
+                <Spinner message="Récupération des données ..." />
+            ) : (
+                children
+            )}
         </AuthContext.Provider>
     );
 };
